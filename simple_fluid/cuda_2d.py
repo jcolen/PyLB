c792251d6386bfd0aaf3a7a7878f17b221565056
Jonathan Colen <jcolen@uchicago.edu>
2021-04-01 18:04:31 -0500

    Improved cuda computation of simple fluid

--- simple_fluid/cuda_2d.py
+++ simple_fluid/cuda_2d.py
@@ -1,65 +1,22 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 from numba import jit, cuda
 from sys import exit
 from argparse import ArgumentParser
 from time import time
-	
-NL = 9
-idxs = np.arange(NL)
-e = np.array([[0, 0],
-			  [0, 1],
-			  [0, -1],
-			  [1, 0],
-			  [-1, 0],
-			  [1, 1],
-			  [1, -1],
-			  [-1, 1],
-			  [-1, -1]]).astype(np.float64)
-
-weights = np.ones(e.shape[0]) / 36.
-weights[1:5] *= 4
-weights[0] *= 16
-bounce = np.array([0, 2, 1, 4, 3, 8, 7, 6, 5]).astype(np.int32)
-
-def build_stream(grid, args):
-	Ny, Nx = grid.shape[1:]
-	prevf = np.empty([Ny, Nx, e.shape[0], 3], dtype=np.int32)
-
-	for i in range(e.shape[0]):
-		prevf[:, :, i, :-1] = np.mod(grid.T - e[i], grid.shape[1:]).transpose(1, 0, 2)
-		prevf[:, :, i, -1] = i
-
-	return prevf
-
-def iterate_evolution(t_max, F1, F2, pattern, prevf, itau_f, bounce=bounce, e=e, weights=weights):
-
-	#Prep cuda info
-	threadsperblock = (32, 32)
-	blockspergrid_y = math.ceil(F1.shape[0] / threadsperblock[0])
-	blockspergrid_x = math.ceil(F1.shape[1] / threadsperblock[1])
-	blockspergrid = (blockspergrid_y, blockspergrid_x)
-
-	#Copy arrays to device
-	d_F1 = cuda.to_device(F1)
-	d_F2 = cuda.to_device(F2)
-	d_pattern = cuda.to_device(pattern)
-	d_prevf = cuda.to_device(prevf)
-	d_bounce = cuda.to_device(bounce)
-	d_e = cuda.to_device(e)
-	d_weights = cuda.to_device(weights)
-
-	for t in range(t_max):
-		evolve_velocity[blockspergrid, threadsperblock](d_F1, d_F2, d_pattern, d_prevf, itau_f, d_bounce, d_e, d_weights)
-		evolve_velocity[blockspergrid, threadsperblock](d_F2, d_F1, d_pattern, d_prevf, itau_f, d_bounce, d_e, d_weights)
-
-	#Copy array back from device
-	d_F1.copy_to_host(F1)
-
-	return F1
 
 @cuda.jit
-def evolve_velocity(F1, F2, pattern, prevf, itau_f, bounce=bounce, e=e, weights=weights):
+def build_stream(prevf, grid, e):
+	y, x = cuda.grid(2)
+	if y < prevf.shape[0] and x < prevf.shape[1]:
+		for i in range(prevf.shape[2]):
+			prevf[y, x, i, 0] = (grid[0, y, x] - e[i, 0]) % prevf.shape[0]
+			prevf[y, x, i, 1] = (grid[1, y, x] - e[i, 1]) % prevf.shape[1]
+			prevf[y, x, i, 2] = i
+
+@cuda.jit
+def evolve_velocity(F1, F2, pattern, prevf, itau_f, bounce, e, weights):
 	y, x = cuda.grid(2)
 	if y < F1.shape[0] and x < F1.shape[1]:
 		if pattern[y, x]:
@@ -110,46 +67,77 @@ if __name__=='__main__':
 	parser.add_argument('--t_write', type=int, default=10)
 	args = parser.parse_args()
 
-	import matplotlib.pyplot as plt
 	fig = plt.figure(figsize=(12, 4))
 	ax = fig.gca()
 	plt.ion()
 	plt.show()
 
+	e = np.array([[0, 0],
+				  [0, 1],
+				  [0, -1],
+				  [1, 0],
+				  [-1, 0],
+				  [1, 1],
+				  [1, -1],
+				  [-1, 1],
+				  [-1, -1]]).astype(np.float64)
+
+	weights = np.ones(e.shape[0]) / 36.
+	weights[1:5] *= 4
+	weights[0] *= 16
+	bounce = np.array([0, 2, 1, 4, 3, 8, 7, 6, 5]).astype(np.int32)
+
 	[Nx, Ny, Nz] = args.shape
 	Y, X = np.mgrid[:Ny, :Nx]
-	grid = np.array([Y, X])
-
-	prevf = build_stream(grid, args)
-	#prevf = [tuple(prevf[i]) for i in range(prevf.shape[0])]
-
-	F1 = np.ones((Ny, Nx, NL), dtype=np.float64) + 0.01 * np.random.randn(Ny, Nx, NL)
+	
+	F1 = 0.01 * np.random.randn(Ny, Nx, e.shape[0]) + 1
 	F1[..., 1] += 2 * (1 + 0.2*np.cos(2 * np.pi*X/Nx*4))
 	F2 = np.empty_like(F1)
 	rho = np.sum(F1, axis=-1)
-	for i in idxs:
-		F1[..., i] *= args.rho / rho
-	
+	F1 *= args.rho / rho[..., None]
+
+	d_F1 = cuda.to_device(F1)
+	F2 = cuda.to_device(np.empty_like(F1))
+	e = cuda.to_device(e)
+	weights = cuda.to_device(weights)
+	bounce = cuda.to_device(bounce)
+
+
+	#Prep cuda info
+	threadsperblock = (32, 32)
+	blockspergrid_y = math.ceil(F1.shape[0] / threadsperblock[0])
+	blockspergrid_x = math.ceil(F1.shape[1] / threadsperblock[1])
+	blockspergrid = (blockspergrid_y, blockspergrid_x)
+
+	prevf = cuda.to_device(np.empty((Ny, Nx, e.shape[0], 3), dtype=np.int32))
+	grid = cuda.to_device(np.mgrid[:Ny, :Nx])
+	build_stream[blockspergrid, threadsperblock](prevf, grid, e)
+
 	if args.pattern:
 		pattern = np.load('pattern.npy').astype(bool)[0]
 	else:
 		pattern = np.zeros([Nz, Ny, Nx], dtype=bool)[0]
 		
+	d_pattern = cuda.to_device(pattern)
+	itau_f = 1. / args.tau_f
+
 	t_current = 0
-
-	t = time()
-
 	while t_current < args.t_max:
 
 		t = time()
-		F1 = iterate_evolution(args.t_write, F1, F2, pattern, prevf, 1.0 / args.tau_f)
+
+		for it in range(args.t_write):
+			evolve_velocity[blockspergrid, threadsperblock](
+				d_F1, F2, d_pattern, prevf, itau_f, bounce, e, weights)
+			evolve_velocity[blockspergrid, threadsperblock](
+				F2, d_F1, d_pattern, prevf, itau_f, bounce, e, weights)
 
 		print(time() - t)
+		d_F1.copy_to_host(F1)
 		rho = np.sum(F1, axis=-1)
 		u = np.divide(np.einsum('abc,cd', F1, e), rho[..., None])
 		img = display(ax, u[..., 1], u[..., 0], pattern=pattern)
 		if input() == 'q':
 			exit(0)
-		t = time()
 
 		t_current += args.t_write
